1c9b6e325557677411b552bc7a2cf8c28d607847
Meredith <meredith.mumby@outlook.com>
2017-08-10 20:00:12 +0000

    Seeded db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170810185556) do
+ActiveRecord::Schema.define(version: 20170810194401) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20170810185556) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.integer  "category_id"
+    t.string   "title"
     t.index ["category_id"], name: "index_posts_on_category_id", using: :btree
   end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,89 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Seeding Data ..."
+
+# Helper functions
+def open_asset(file_name)
+  File.open(Rails.root.join('db', 'seed_assets', file_name))
+end
+
+# Only run on development (local) instances not on production, etc.
+unless Rails.env.development?
+  puts "Development seeds only (for now)!"
+  exit 0
+end
+
+puts "Finding or Creating Categories ..."
+
+
+# categories
+
+cat1 = Category.find_or_create_by! name: 'Left overs'
+cat2 = Category.find_or_create_by! name: 'Garden extras'
+cat3 = Category.find_or_create_by! name: 'Wild produce'
+cat4 = Category.find_or_create_by! name: 'Grocery store extras'
+cat5 = Category.find_or_create_by! name: 'Other'
+
+# posts
+
+puts "Re-creating posts"
+
+Post.destroy_all
+
+cat1.posts.create!({
+  title: "Veggies es bonus vobis",
+  description: "Veggies es bonus vobis, proinde vos postulo essum magis kohlrabi welsh onion daikon amaranth tatsoi tomatillo melon azuki bean garlic.",
+  image: "http://i.imgur.com/ToFylpO.jpg"
+  })
+
+cat1.posts.create!({
+  title: "gumbo gourd",
+  description: "Gumbo beet greens corn soko endive gumbo gourd. Parsley shallot courgette tatsoi pea sprouts fava bean collard greens dandelion okra wakame tomato.",
+  image: "http://i.imgur.com/Ay8s20X.jpg"
+  })
+
+cat1.posts.create!({
+  title: "greens yarrow ricebean",
+  description: "Turnip greens yarrow ricebean rutabaga endive cauliflower sea lettuce kohlrabi amaranth water spinach avocado daikon napa cabbage asparagus winter purslane kale.",
+  image: "http://i.imgur.com/CW5GHuv.jpg"
+  })
+
+cat2.posts.create!({
+  title: "Parsley shallot courgette",
+  description: "Veggies es bonus vobis, proinde vos postulo essum magis kohlrabi welsh onion daikon amaranth tatsoi tomatillo melon azuki bean garlic.",
+  image: "http://i.imgur.com/feYPs6R.jpg"
+  })
+
+cat2.posts.create!({
+  title: "rutabaga endive",
+  description: "Gumbo beet greens corn soko endive gumbo gourd. Parsley shallot courgette tatsoi pea sprouts fava bean collard greens dandelion okra wakame tomato.",
+  image: "http://i.imgur.com/Wy94Tt7.jpg"
+  })
+
+cat2.posts.create!({
+  title: "icebean rutabaga",
+  description: "Turnip greens yarrow ricebean rutabaga endive cauliflower sea lettuce kohlrabi amaranth water spinach avocado daikon napa cabbage asparagus winter purslane kale.",
+  image: "http://i.imgur.com/jeJ3tYO.jpg"
+  })
+
+cat3.posts.create!({
+  title: "essum magis kohlrabi",
+  description: "Veggies es bonus vobis, proinde vos postulo essum magis kohlrabi welsh onion daikon amaranth tatsoi tomatillo melon azuki bean garlic.",
+  image: "http://i.imgur.com/4KGVioH.jpg"
+  })
+
+cat3.posts.create!({
+  title: "greens corn",
+  description: "Gumbo beet greens corn soko endive gumbo gourd. Parsley shallot courgette tatsoi pea sprouts fava bean collard greens dandelion okra wakame tomato.",
+  image: "http://i.imgur.com/ZgjA8QQ.jpg"
+  })
+
+cat3.posts.create!({
+  title: "cauliflower sea lettuce",
+  description: "Turnip greens yarrow ricebean rutabaga endive cauliflower sea lettuce kohlrabi amaranth water spinach avocado daikon napa cabbage asparagus winter purslane kale.",
+  image: "http://i.imgur.com/LU0r5dG.jpg"
+  })
+
+
