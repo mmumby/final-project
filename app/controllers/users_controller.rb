3b87e3146e8d231cedccb969036c58f3f621ffa7
Meredith <meredith.mumby@outlook.com>
2017-08-12 00:47:28 +0000

    showed posts on category page and user profile page, changed user profile view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,6 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
+    @posts = Post.joins(:user).where(:posts => { :user_id => @user.id })
   end
-
-
 end
