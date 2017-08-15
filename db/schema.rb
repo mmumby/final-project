2d0f6ecf24060aa9fb221dd7cdcc22bed0ac5be1
stevetice <tice.steve@gmail.com>
2017-08-15 19:52:09 +0000

    Create migration to add Lat and Long fields to posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170815160232) do
+ActiveRecord::Schema.define(version: 20170815194812) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,6 +51,8 @@ ActiveRecord::Schema.define(version: 20170815160232) do
     t.string   "title"
     t.integer  "user_id"
     t.string   "location"
+    t.float    "latitude"
+    t.float    "longitude"
     t.index ["category_id"], name: "index_posts_on_category_id", using: :btree
     t.index ["user_id"], name: "index_posts_on_user_id", using: :btree
   end
