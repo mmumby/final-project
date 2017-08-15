26502bc666c0ae6033693add9564c3393510bf3d
stevetice <tice.steve@gmail.com>
2017-08-15 17:11:05 +0000

    Created migration adding location field to posts table, changed google static map location

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-
-ActiveRecord::Schema.define(version: 20170813191147) do
+ActiveRecord::Schema.define(version: 20170815160232) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -52,6 +50,7 @@ ActiveRecord::Schema.define(version: 20170813191147) do
     t.integer  "category_id"
     t.string   "title"
     t.integer  "user_id"
+    t.string   "location"
     t.index ["category_id"], name: "index_posts_on_category_id", using: :btree
     t.index ["user_id"], name: "index_posts_on_user_id", using: :btree
   end
