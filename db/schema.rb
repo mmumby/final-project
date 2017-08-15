24f2e4c77fb35c5c6969f74e1514f43390643ae5
Meredith <meredith.mumby@outlook.com>
2017-08-15 16:25:10 +0000

    settled merge conflict

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 # It's strongly recommended that you check this file into your version control system.
 
 
-ActiveRecord::Schema.define(version: 20170815161605) do
+ActiveRecord::Schema.define(version: 20170813191147) do
 
 
   # These are extensions that must be enabled in order to support this database
