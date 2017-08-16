d0830bd3c293189991acc7503a3ec2ce90337366
stevetice <tice.steve@gmail.com>
2017-08-16 19:32:24 +0000

    Add validations and error messages for new posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,6 +4,13 @@ class Post < ApplicationRecord
   has_many :comments, as: :commentable
 
   validates :category, presence: true
+  validates :description, length: { in: 40..280,
+    too_long: "%{count} character is the maximum allowed",
+    too_short: "%{count} character is the minimum required" }
+  validates :title, length: { in: 10..40,
+    too_long: "%{count} character is the maximum allowed",
+    too_short: "%{count} character is the minimum required" }
+  validates :location, presence: true
 
   geocoded_by :location
   after_validation :geocode, :if => :location_changed?
