c78eb86698f25e6cbc5a8b582a7999739e2c5d03
Mathew Kostiuk <mathewkostiuk@gmail.com>
2017-08-16 21:48:52 +0000

    implemented notifications on message sent

--- app/jobs/message_broadcast_job.rb
+++ app/jobs/message_broadcast_job.rb
@@ -2,7 +2,7 @@ class MessageBroadcastJob < ApplicationJob
   queue_as :default
 
   def perform(message)
-    ActionCable.server.broadcast "room-#{message.chatroom_id}:messages", message: render_message(message)
+    ActionCable.server.broadcast "room-#{message.chatroom_id}:messages", message: render_message(message), notification: render_notification(message)
   end
 
   private
