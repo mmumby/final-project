0cffca0f0a2306e45316f7269c133aa80f8bc216
Mathew Kostiuk <mathewkostiuk@gmail.com>
2017-08-16 20:38:02 +0000

    added recipient_id to messages table, implemented check_recipient method in messages channel

--- app/channels/messages_channel.rb
+++ app/channels/messages_channel.rb
@@ -4,6 +4,17 @@ class MessagesChannel < ApplicationCable::Channel
   end
 
   def receive(payload)
-    Message.create(user: current_user, chatroom_id: payload["chatroom_id"], content: payload["message"])
+    @chatroom = Chatroom.find(payload["chatroom_id"])
+    Message.create(user: current_user, chatroom_id: payload["chatroom_id"], content: payload["message"], recipient_id: check_recipient(@chatroom.id))
+  end
+
+  def check_recipient(chatroom)
+    @chatroom = Chatroom.find(chatroom)
+    if
+      current_user != @chatroom.owner_id
+      @chatroom.client_id
+    else
+      @chatroom.owner_id
+    end
   end
 end
--- app/jobs/message_broadcast_job.rb
+++ app/jobs/message_broadcast_job.rb
@@ -9,4 +9,8 @@ class MessageBroadcastJob < ApplicationJob
     def render_message(message)
       ApplicationController.renderer.render(partial: 'messages/message', locals: { message: message })
     end
+
+    def render_notification(message)
+      ApplicationController.renderer.render(partial: 'notifications/notification', locals: { message: message })
+    end
 end
