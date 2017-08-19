926025553e4f4518c5853fe8ea4d9bd1d43ec626
stevetice <tice.steve@gmail.com>
2017-08-19 06:30:05 +0000

    Updated map helper, removed busnisess, attractions, palces or worship

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,7 +5,7 @@ module ApplicationHelper
     center = "#{obj.latitude},#{obj.longitude}"
     query = defaults.merge config.merge center: center, key: ENV['GOOGLE_STATIC_MAP_KEY']
     query[:size] = "#{query[:width]}x#{query[:height]}"
-    url = "https://maps.googleapis.com/maps/api/staticmap?#{query.to_query}"
+    url = "https://maps.googleapis.com/maps/api/staticmap?#{query.to_query}&style=feature:poi.attraction|visibility:off&style=feature:poi.business|visibility:off&style=feature:poi.place_of_worship|visibility:off"
 
     "<img src=\"#{url}\" />".html_safe
   end
