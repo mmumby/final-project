98736080f563548440e1106d79efc9afe2593771
Meredith <meredith.mumby@outlook.com>
2017-08-21 22:54:34 +0000

    updated expire logic

--- db/schema.rb
+++ db/schema.rb
@@ -100,22 +100,22 @@ ActiveRecord::Schema.define(version: 20170816200847) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
+    t.string   "email",                  default: "",                                   null: false
+    t.string   "encrypted_password",     default: "",                                   null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",          default: 0,                                    null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.inet     "current_sign_in_ip"
     t.inet     "last_sign_in_ip"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
+    t.datetime "created_at",                                                            null: false
+    t.datetime "updated_at",                                                            null: false
     t.string   "provider"
     t.string   "uid"
     t.string   "name"
-    t.text     "image"
+    t.text     "image",                  default: "/images/default_profile_normal.png"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
