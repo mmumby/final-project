cebc87380c9cd0ef9e039fb112fbc65a9ed2e3cd
Meredith <meredith.mumby@outlook.com>
2017-08-23 22:33:53 +0000

    changed facebook callback

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -254,7 +254,7 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
-  config.omniauth :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'], callback_url: "https://boiling-spire-30627.herokuapp.com/users/auth/facebook/callback"
+  config.omniauth :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'], callback_url: "http://lettuceshare.ca/users/auth/facebook/callback"
   config.omniauth :twitter, ENV['TWITTER_API'], ENV['TWITTER_KEY'], scope: 'email,profile,online'
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
